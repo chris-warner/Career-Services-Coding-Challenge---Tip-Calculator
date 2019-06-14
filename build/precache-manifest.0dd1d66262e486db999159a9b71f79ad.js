1e069d9e63bd78cfeebd9b94027ee69eda589041
Chris Warner <christopher.warner92@gmail.com>
2019-06-14 10:25:57 -0400

    build

---
+++ build/precache-manifest.0dd1d66262e486db999159a9b71f79ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7531b7e1d20ad33fe5864227e18f20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce315689530040f10380",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7f449af311a5586df5fc",
+    "url": "/static/js/2.de9c2eec.chunk.js"
+  },
+  {
+    "revision": "ce315689530040f10380",
+    "url": "/static/js/main.a84c167f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8cf47f0020d5ca9aab00bd1407743978",
+    "url": "/static/media/tip.8cf47f00.jpeg"
+  }
+]);
