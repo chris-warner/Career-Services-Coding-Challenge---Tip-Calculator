4877760e7b86dd0f7235e9dd818ee9e6bec9392d
Chris Warner <christopher.warner92@gmail.com>
2019-06-14 07:44:40 -0400

    added a Jumbotron Component

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavbarComponent from './components/NavbarComponent';
 import TipCalculatorForm from './components/TipCalculatorForm';
+import JumbotronComponent from './components/JumbotronComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <JumbotronComponent />
     <NavbarComponent title={'Tip Calculator'} />
     <TipCalculatorForm  />
     </div>
---
+++ tip-calculator/src/components/JumbotronComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Jumbotron} from 'react-bootstrap';
+
+function JumbotronComponent() {
+    return(
+        <Jumbotron>
+        Tip Calculator is a simple application that will help calculate a tip at a restaurant for your server.
+        </Jumbotron>
+    );
+}
+
+export default JumbotronComponent;
