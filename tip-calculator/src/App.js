49b13250be17a43854702ac5ff7c45e62af74258
Chris Warner <christopher.warner92@gmail.com>
2019-06-14 07:31:47 -0400

    added navbar component as well as react-bootstrap for better styling

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavbarComponent from './components/NavbarComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav style={navStyle}>
+        <ul>
+          <il>Tip Calculator</il>
+        </ul>
+      </nav>
     </div>
   );
+}
+
+const navStyle = {
+  //height:'80px',
+  backgroundColor:'blue'
 }
 
 export default App;
