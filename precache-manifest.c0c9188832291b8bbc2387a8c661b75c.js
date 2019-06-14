26ff6613cc6b605721d9aaebc969a594c972975d
Chris Warner <christopher.warner92@gmail.com>
2019-06-14 10:11:54 -0400

    Updates

---
+++ precache-manifest.c0c9188832291b8bbc2387a8c661b75c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22d7c582e8f171b958bb0ec2f9aaab4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c5e9255f054d66b9267",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7f449af311a5586df5fc",
+    "url": "/static/js/2.de9c2eec.chunk.js"
+  },
+  {
+    "revision": "4c5e9255f054d66b9267",
+    "url": "/static/js/main.6594f9cf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8cf47f0020d5ca9aab00bd1407743978",
+    "url": "/static/media/tip.8cf47f00.jpeg"
+  }
+]);
